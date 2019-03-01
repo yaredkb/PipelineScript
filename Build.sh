2e74c88a5f6f99f967c1d440d516f600024f4448
Yared Bokru <yaredk@sbcglobal.net>
2019-03-01 16:28:54 -0500

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,2 +1,3 @@
-echo "Building the Project : %date% : %time%"
-date +'%d/%m/%Y %H:%M:%S'
+echo Building the Project : %date% : %time%
+Building the Project : date +%H:%M:%S
+date +%H:%M:%Sdate +'%d/%m/%Y %H:%M:%S'
