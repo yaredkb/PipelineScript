1b585469d5b903ced567ea35f72cd2f6e9481272
Yared Bokru <yaredk@sbcglobal.net>
2019-03-04 09:11:46 -0500

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,3 @@
 echo "Building the Project :"  
 date +'%d/%m/%Y %H:%M:%S'
-
-#Syntax to define a string variable
-str='Current date is : dd=$(date -v -1d '+%m-%d-%y')
-echo $str
+dd=$(date -v -1d '+%m-%d-%y')
